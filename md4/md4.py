a97124f17e74bf52629c12eee1181d0d88bb6825
njaladan <ganesh.jaladanki@gmail.com>
2018-03-31 23:17:19 -0700

    finished md4

--- md4/md4.py
+++ md4/md4.py
@@ -4,15 +4,6 @@
 #
 import struct
 
-def convert_to_64bit(data):
-    bytearr = bytearray(64)
-    count = 63
-    while data:
-        bytearr[count] = data % 256
-        data = int((data - (data % 256)) / 256)
-        count -= 1
-    return bytearr
-
 def f(x,y,z):
     return x & y | ~x & z
 
@@ -26,7 +17,6 @@ def leftrotate(n, b):
     #print(n,b)
     return ((n << b) | ((n & 0xffffffff) >> (32 - b))) & 0xffffffff
 
-
 class md4():
 
     def __init__(self, data=None):
@@ -38,13 +28,13 @@ class md4():
                 0x98badcfe,
                 0x10325476
                 ]
-        
+
         if data != None:
             self.update(data)
 
 
     def update(self, bytestring):
-        
+
         if type(bytestring) != bytes:
             bytestring = str.encode(bytestring)
         bytestring = bytearray(bytestring)
@@ -52,16 +42,15 @@ class md4():
         self.data += bytestring
 
         # step 1: padding bits
-        numtopad = 48 - (len(bytestring) % 64)
+        numtopad = 56 - (len(bytestring) % 64)
         if numtopad <= 0:
             numtopad += 64
 
         padded = self.data + bytearray([128])
         padded += bytearray(numtopad - 1)
 
-        
         # step 2: append length
-        padded += convert_to_64bit(len(self.data) * 8)
+        padded += (len(self.data) * 8).to_bytes(8, 'little', signed=False)
 
         # step 3: process message
         vals = [0x67452301,0xefcdab89,0x98badcfe,0x10325476]
@@ -71,8 +60,9 @@ class md4():
             valprev = vals[:]
             X = []
             for j in range(0, 64, 4):
-                X.append(int.from_bytes(padded[64*i + j:64*i + j + 4], 'little', signed=False)) 
-            
+                X.append(int.from_bytes(padded[64*i + j:64*i + j + 4], 'little', signed=False))
+
+            # round 1
             s = (3,7,11,19)
             for i in range(16):
                 r = (16-i)%4
@@ -81,13 +71,9 @@ class md4():
                 b = vals[(r + 1) % 4]
                 c = vals[(r + 2) % 4]
                 d = vals[(r + 3) % 4]
-                if i>12:
-                    print(f(b,c,d), X)
-                    print(a + f(b,c,d) + X[k], s[k % 4])
                 vals[r] = leftrotate(a + f(b,c,d) + X[k], s[k % 4])
-                
-            print(list(map(hex, vals)))
-            
+
+            # round 2
             s = (3,5,9,13)
             for i in range(16):
                 div, mod = divmod(i,4)
@@ -99,27 +85,27 @@ class md4():
                 d = vals[(r + 3) % 4]
                 vals[r] = leftrotate(a + g(b,c,d) + X[k] + 0x5A827999, s[i % 4])
 
-            print(list(map(hex, vals)))
+            # round 3
             s = (3,9,11,15)
-            k = (0,8,4,12,2,10,6,14,1,9,5,13,3,11,7,15)
+            k_ls = (0,8,4,12,2,10,6,14,1,9,5,13,3,11,7,15)
             for i in range(16):
-                
+
                 r = (16-i)%4
-                k = i
+                k = k_ls[i]
                 a = vals[r]
                 b = vals[(r + 1) % 4]
                 c = vals[(r + 2) % 4]
                 d = vals[(r + 3) % 4]
 
-                vals[r] = leftrotate(a + h(b,c,d) + X[k] + 0x6ED9EBA1, s[k % 4])
+                vals[r] = leftrotate(a + h(b,c,d) + X[k] + 0x6ED9EBA1, s[i % 4])
 
-            print(list(map(hex, vals)))
+            # register additions
             for i in range(4):
                 vals[i] = (valprev[i] + vals[i]) % 2**32
 
         self.vals = vals
-        return self    
-        
+        return self
+
     @property
     def digest(self):
         return bytes(self.hash)
@@ -131,7 +117,17 @@ class md4():
             bh = bh + bytearray(struct.pack('<I', self.vals[i]))
         return bh.hex()
 
+# Binary strings contain bytewise data, similar to a bytearray
+test_vectors = [b"",
+                b"a",
+                b"abc",
+                b"message digest",
+                b"abcdefghijklmnopqrstuvwxyz",
+                b"ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789",
+                b"12345678901234567890123456789012345678901234567890123456789012345678901234567890"]
+def main():
+    for vector in test_vectors:
+        print('MD4 ("{0}") = {1}'.format(vector.decode(), md4(data=vector).hexdigest))
 
-a = md4(b'a')
-print(a.hexdigest)
-        
+if __name__ == '__main__':
+    main()
